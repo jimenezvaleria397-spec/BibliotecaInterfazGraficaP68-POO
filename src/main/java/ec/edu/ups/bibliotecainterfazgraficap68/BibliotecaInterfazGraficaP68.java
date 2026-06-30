fa95b6ab5a99c527d9be8983df8021f2744d4ecd
jimen <jimenezvaleria397@gmail.com>
2026-06-29 19:56:26 -0500

    "Inicializacion del proyecto "

---
+++ src/main/java/ec/edu/ups/bibliotecainterfazgraficap68/BibliotecaInterfazGraficaP68.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ups.bibliotecainterfazgraficap68;
+
+/**
+ *
+ * @author jimen
+ */
+public class BibliotecaInterfazGraficaP68 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
