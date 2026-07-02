58345b97413495e53ee95e07be3715e74f0730d3
jimen <jimenezvaleria397@gmail.com>
2026-07-02 00:32:44 -0500

    "Actualizaciones"

--- src/main/java/ec/edu/ups/biblioteca/models/Prestamo.java
+++ src/main/java/ec/edu/ups/biblioteca/models/Prestamo.java
@@ -4,10 +4,79 @@
  */
 package ec.edu.ups.biblioteca.models;
 
+import java.util.Date;
+
 /**
  *
  * @author jimen
  */
 public class Prestamo {
-    
+
+    private String codigo;
+    private Usuario usuario;
+    private Libro libro;
+    private Date fechaPrestamo;
+    private Date fechaDevolucion;
+    private String estado;
+
+    public Prestamo() {
+    }
+
+    public Prestamo(String codigo, Usuario usuario, Libro libro,
+                    Date fechaPrestamo, Date fechaDevolucion, String estado) {
+        this.codigo = codigo;
+        this.usuario = usuario;
+        this.libro = libro;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+        this.estado = estado;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public Date getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(Date fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public Date getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(Date fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
